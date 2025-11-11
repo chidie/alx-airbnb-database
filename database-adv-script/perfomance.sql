acef3fba4998d53a67fa1922c3e3c089b5818605
Chidiebere Onuoha <chidieonuoha@gmail.com>
2025-11-11 23:17:15 +0100

    made some corrections

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,6 +1,7 @@
 --- Write an initial query that retrieves all bookings along with the user details, property details, and payment details
 
-EXPLAIN ANALYZE SELECT 
+EXPLAIN ANALYZE
+SELECT 
   b.booking_id,
   b.start_date,
   b.end_date,
@@ -26,4 +27,9 @@ FROM airbnb_schema.bookings b
 JOIN airbnb_schema.users u ON b.user_id = u.user_id
 JOIN airbnb_schema.properties p ON b.property_id = p.property_id
 LEFT JOIN airbnb_schema.payments pay ON b.booking_id = pay.booking_id
+
+WHERE b.status = 'confirmed'
+AND b.created_at >= '2025-01-01'
+
 ORDER BY b.created_at DESC;
+
